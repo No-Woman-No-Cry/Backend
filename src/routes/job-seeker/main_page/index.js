30e1d52e1b42fa304c2c75a0c7cd6549f3d7df69
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-17 01:10:21 +0700

    routiung ke /main/categories

--- src/routes/job-seeker/main_page/index.js
+++ src/routes/job-seeker/main_page/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const MainPageController = require("@controllers/job-seeker/MainPageController");
 // Routing main page disini
 router.post("/jobs", MainPageController.getAllJobs);
+router.get("/categories", MainPageController.getCategories);
 
 module.exports = router;
