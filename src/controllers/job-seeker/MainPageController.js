1ab08f625d4a30090fae36f05358fe8d9f50a7ca
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-17 00:42:49 +0700

    Menyelesaikan getAllJobs()

--- src/controllers/job-seeker/MainPageController.js
+++ src/controllers/job-seeker/MainPageController.js
@@ -1,4 +1,8 @@
 const Job = require("@models").Job;
+const Company = require("@models").Company;
+const JobSalary = require("@models").JobSalary;
+const JobTypeRequirement = require("@models").JobTypeRequirement;
+const JobType = require("@models").JobType;
 class MainPageController {
   static async getAllJobs(req, res) {
     try {
@@ -9,16 +13,63 @@ class MainPageController {
       const totalPages = Math.ceil(totalItems / itemsPerPage);
 
       const jobs = await Job.findAll({
+        attributes: ["id", "job_position", "job_work_place"],
         limit: itemsPerPage,
         offset: (currentPage - 1) * itemsPerPage,
+        include: [
+          {
+            model: Company,
+            attributes: ["company_name", "company_icon", "location"],
+          },
+          {
+            model: JobSalary,
+            attributes: ["minimum_salary", "maximum_salary"],
+          },
+          {
+            model: JobType,
+            as: "jobType",
+            // attributes: ["minimum_salary", "maximum_salary"],
+          },
+        ],
       });
+
+      // Transformasi jobs
+      const transformedJobs = jobs.map((job) => {
+        let jobTypeString = "";
+        const transformJobType = job.jobType
+          .map((jt) => jt.job_type_name)
+          .join(" , ");
+        /**
+         "id": 1,
+          "job_title": "Software Engineer",
+          "company_name": "PT XYZ Technology",
+          "location": "Jakarta",
+          "job_type": "Full-time",
+          "salary": "Rp 15.000.000",
+          "work_location": "On-site"
+         */
+        const { id, job_position, job_work_place } = job;
+        return {
+          id: id,
+          job_title: job_position,
+          company_name: job.Company.company_name,
+          company_icon: job.Company.company_icon,
+          location: job.Company.location,
+          job_type: transformJobType,
+          salary: `${job.JobSalary.minimum_salary} - ${job.JobSalary.maximum_salary}`,
+          work_location: job_work_place,
+        };
+      });
+
+      // console.log(jobs[0].jobType[0].job_type_name);
 
       const response = {
         page: currentPage,
         total_pages: totalPages,
         total_jobs: totalItems,
-        jobs: jobs,
+        jobs: transformedJobs,
       };
+
       return res.status(200).json({
         code: 200,
         success: true,
