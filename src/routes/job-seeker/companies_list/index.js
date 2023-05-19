8076dfde9a3eb59a19a4a4e7da73fc59a381f418
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-20 00:59:06 +0700

    Membenahi route companies list

--- src/routes/job-seeker/companies_list/index.js
+++ src/routes/job-seeker/companies_list/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const CompanyPage = require("../../../controllers/job-seeker/CompanyController");
+const CompanyPageController = require("@controllers/job-seeker/CompanyController");
 const router = express.Router();
 // Routing companies list disini
-router.get("/company", CompanyPage.companylist);
-router.get("/company", CompanyPage.companydetail);
+router.get("/", CompanyPageController.companylist);
+router.get("/:id", CompanyPageController.companydetail);
 module.exports = router;
