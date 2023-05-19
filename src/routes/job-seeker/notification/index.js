d06cd6cfab6fb6ecce3d7d3e6fda9d2e6627eba7
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-20 01:08:12 +0700

    Ubah route notification

--- src/routes/job-seeker/notification/index.js
+++ src/routes/job-seeker/notification/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const NotificationController = require ("@controllers/job-seeker/NotificationController.js")
+const NotificationController = require("@controllers/job-seeker/NotificationController.js");
 // Routing notifikasi disini
 
-router.get("/notification", NotificationController.notification);
+router.get("/:profile_id", NotificationController.notification);
 module.exports = router;
