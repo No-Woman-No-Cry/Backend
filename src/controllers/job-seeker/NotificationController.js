6e9ec4dda36af3bc50e07151cda30b31e4ce26d4
Ridho Pujiono <68780041+ridhopujiono@users.noreply.github.com>
2023-05-20 01:09:17 +0700

    Menyelesaikan koresi API Notifikasi dan Companies List (#22)
    * Mengkoreksi API Education dan Profile
    
    * Mengkoresi Company Controller
    
    * Membenahi route companies list
    
    * Mengkoreksi Notification Koreksi
    
    * Ubah route notification

--- src/controllers/job-seeker/NotificationController.js
+++ src/controllers/job-seeker/NotificationController.js
@@ -1,23 +1,32 @@
 const UserNotifications = require("@models").UserNotifications;
 
 class NotificationController {
-    static async notification (req, res) {
-        try {
-            const id = req.params.id
-            const data = await UserNotifications.findByPK(id)
-            return res.status(200).json({
-                code: 200,
-                success: true,
-                message: "Notification Fetched",
-                data: data,
-            })
-        } catch (error) {
-            return res.status(500).json({
-                code: 500,
-                success: false,
-                message: "Error Message Here",
-            })
-        }
+  static async notification(req, res) {
+    try {
+      const profile_id = req.params.profile_id;
+      const data = await UserNotifications.findAll({
+        attributes: ["notification_message"],
+        where: {
+          user_id: profile_id,
+        },
+      });
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: "Notification Fetched",
+        data: data.map((d) => {
+          return {
+            message: d.notification_message,
+          };
+        }),
+      });
+    } catch (error) {
+      return res.status(500).json({
+        code: 500,
+        success: false,
+        message: error.message,
+      });
     }
+  }
 }
 module.exports = NotificationController;
