232b586143fd74fdb780b9c832987360423c4fd9
saqfaalam <saqfaalam18@gmail.com>
2023-05-22 18:42:59 +0700

    menambahkan file baru pada employer

--- src/controllers/employer/IndustryController.js
+++ src/controllers/employer/IndustryController.js
@@ -1,8 +1,20 @@
+const { Industry } = require('@models'); // Import model Industry
+
 class IndustryController {
   static async getAllIndustry(req, res) {
-    return res.json({
-      message: "getAllIndustry() has accessed",
-    });
+    try {
+      const industries = await Industry.findAll(); // Mengambil semua data Industry
+
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: 'Industries Fetched',
+        data: industries,
+      });
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 }
 
