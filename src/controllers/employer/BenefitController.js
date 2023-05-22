3cce7f2828aacf6b7090220142ae41f2d252b846
saqfaalam <121917797+saqfaalam@users.noreply.github.com>
2023-05-22 19:07:34 +0700

    Saqfaalam (#33)
    * Profile & Education tolong di cek mas
    
    * Inisiasi controller untuk company
    
    * Inisiasi Ulang Company Controller
    
    * menambahkan file baru pada employer
    
    ---------
    
    Co-authored-by: Ridho Pujiono <ridho@smktjp.sch.id>
    Co-authored-by: Ridho Pujiono <68780041+ridhopujiono@users.noreply.github.com>

--- src/controllers/employer/BenefitController.js
+++ src/controllers/employer/BenefitController.js
@@ -1,8 +1,20 @@
+const { Benefit } = require('@models'); // Import model Benefit
+
 class BenefitController {
   static async getAllBenefit(req, res) {
-    return res.json({
-      message: "getAllBenefit() has accessed",
-    });
+    try {
+      const benefits = await Benefit.findAll(); // Mengambil semua data Benefit
+
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: 'Benefit Fetched',
+        data: benefits,
+      });
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 }
 
--- src/controllers/employer/SalaryController.js
+++ src/controllers/employer/SalaryController.js
@@ -1,8 +1,20 @@
+const { JobSalary } = require('@models'); // Import model JobSalary
+
 class SalaryController {
   static async getAllSalary(req, res) {
-    return res.json({
-      message: "getAllSalary() has accessed",
-    });
+    try {
+      const salaries = await JobSalary.findAll(); // Mengambil semua data JobSalary
+
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: 'Job Salaries Fetched',
+        data: salaries,
+      });
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 }
 
