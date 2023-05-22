9ac64f890e457de37c7bb21fa1f8046acd7c68d9
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-22 20:10:52 +0700

    Transform data dari fetch tabel

--- src/controllers/employer/SalaryController.js
+++ src/controllers/employer/SalaryController.js
@@ -1,19 +1,27 @@
-const { JobSalary } = require('@models'); // Import model JobSalary
+const { JobSalary } = require("@models"); // Import model JobSalary
 
 class SalaryController {
   static async getAllSalary(req, res) {
     try {
-      const salaries = await JobSalary.findAll(); // Mengambil semua data JobSalary
-
+      const salaries = await JobSalary.findAll({
+        attributes: ["id", "minimum_salary", "maximum_salary"],
+      }); // Mengambil semua data JobSalary
+      const transformedSalaries = salaries.map((salary) => {
+        return {
+          salary_id: salary.id,
+          min_salary: salary.minimum_salary,
+          max_salary: salary.maximum_salary,
+        };
+      });
       return res.status(200).json({
         code: 200,
         success: true,
-        message: 'Job Salaries Fetched',
-        data: salaries,
+        message: "Job Salaries Fetched",
+        data: transformedSalaries,
       });
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ message: 'Internal server error' });
+      console.error("Error:", error);
+      return res.status(500).json({ message: "Internal server error" });
     }
   }
 }
