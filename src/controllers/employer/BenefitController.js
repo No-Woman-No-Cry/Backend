e69dbc4acedade7db63e2d8b2b450964efb571fe
Ridho Pujiono <68780041+ridhopujiono@users.noreply.github.com>
2023-05-22 20:14:22 +0700

    Transform hasil fetching tabel (#34)
    * Mengkoreksi API Education dan Profile
    
    * Mengkoresi Company Controller
    
    * Membenahi route companies list
    
    * Mengkoreksi Notification Koreksi
    
    * Ubah route notification
    
    * Hapus controller yang tidak terpakai
    
    * Basic Info My Company API
    
    * Menambah cors
    
    * Ubah posisi cors
    
    * Routing untuk Company controller
    
    * Berhasil membuat autentikasi dan regsitrasi Employer
    
    * Menambah dan display company benefits
    
    * Berhasil membuat add dan display company industries
    
    * Transform data dari fetch tabel

--- src/controllers/employer/BenefitController.js
+++ src/controllers/employer/BenefitController.js
@@ -1,19 +1,25 @@
-const { Benefit } = require('@models'); // Import model Benefit
+const { Benefit } = require("@models"); // Import model Benefit
 
 class BenefitController {
   static async getAllBenefit(req, res) {
     try {
       const benefits = await Benefit.findAll(); // Mengambil semua data Benefit
-
+      const transformedBenefits = benefits.map((benefit) => {
+        return {
+          benefit_id: benefit.id,
+          name: benefit.benefit_name,
+          icon: benefit.icon,
+        };
+      });
       return res.status(200).json({
         code: 200,
         success: true,
-        message: 'Benefit Fetched',
-        data: benefits,
+        message: "Benefit Fetched",
+        data: transformedBenefits,
       });
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ message: 'Internal server error' });
+      console.error("Error:", error);
+      return res.status(500).json({ message: "Internal server error" });
     }
   }
 }
