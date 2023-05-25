29e54aa456250e5784d0d5f3366e219e9b14b47b
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-25 20:49:59 +0700

    Hapus id 4 pada seeder

--- seeders/20230510151707-JobSkill.js
+++ seeders/20230510151707-JobSkill.js
@@ -33,12 +33,6 @@ module.exports = {
         updated_at: new Date(),
       },
       {
-        skill_id: 4,
-        job_id: 2,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
         skill_id: 8,
         job_id: 3,
         created_at: new Date(),
